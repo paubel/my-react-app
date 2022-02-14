b29a5fa58f7247ba0ac403041d816c3fdc573286
paubel <paul.belfrage@gmail.com>
2022-02-14 12:49:59 +0100

    test JS function

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ import "./index.css";
 //import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const name = "Josh Perez";
-const element = <h1>Hello, {name}</h1>;
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Paul",
+  lastName: "Belfrage",
+};
+
+const element = <h1>Hello, {formatName(user)}!</h1>;
 
 ReactDOM.render(element, document.getElementById("root"));
 
-ReactDOM.render(
-  element,
-  document.getElementById("root")
-  /*  <React.StrictMode>
+/* ReactDOM.render(
+
+   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root") */
-);
+  document.getElementById("root")
+); */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
